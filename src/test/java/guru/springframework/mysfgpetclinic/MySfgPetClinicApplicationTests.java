9fc42aa6a056e15b5cee1eff39bf551f958f7d4c
Van Dillon <vdillon@edge2web.com>
2018-09-01 14:20:43 -0400

    Initial checkin

---
+++ src/test/java/guru/springframework/mysfgpetclinic/MySfgPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.mysfgpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
